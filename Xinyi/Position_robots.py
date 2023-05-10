2d7ace454d7226d8ce6bfebe627e3cfc7859c918
Xinyi25 <xinyi.zhao1225@gmail.com>
2023-05-10 18:02:55 +0200

    position_robot + gaussian distribution plot + partition with robot

--- Xinyi/Position_robots.py
+++ Xinyi/Position_robots.py
@@ -8,7 +8,13 @@ def lloyd_algorithm(data, k, num_iterations=60):
 
     # Define Gaussian density function
     cov = np.cov(data.T)
-    mvn = multivariate_normal(mean=[1.8, 0.8], cov=cov)
+    poids_centrale = [1, 0.5]
+    mvn = multivariate_normal(poids_centrale, cov=cov)
+
+    # Evaluate the density function at each point on the grid
+    rv = multivariate_normal(mean=poids_centrale, cov=cov)
+    z = rv.pdf(pos)
+    
 
     # Initialize centroids randomly
     centroids = np.zeros((k, data.shape[1]))
@@ -26,7 +32,7 @@ def lloyd_algorithm(data, k, num_iterations=60):
     # Run the algorithm for the specified number of iterations
     for i in range(num_iterations):
         # Assign data points to the nearest centroid
-        distances = np.linalg.norm(data[:, np.newaxis, :] - centroids, axis=2)
+        distances = np.linalg.norm(data[:, np.newaxis, :] - robot_positions, axis=2)
         labels = np.argmin(distances, axis=1)
 
         # Update centroids to the mean of assigned data points
@@ -46,7 +52,7 @@ def lloyd_algorithm(data, k, num_iterations=60):
             if distances == 0:
                 step_size = 1
             else:
-                step_size = 0.1
+                step_size = 0.3
 
             if distances < step_size: # if distance is less than step size, move the robot to the centroid
                 robot_positions[j] = centroids[j]
@@ -58,8 +64,10 @@ def lloyd_algorithm(data, k, num_iterations=60):
         # Update plot
         scatter.set_offsets(data)
         scatter.set_color([plt.cm.tab10(i) for i in labels])
+        ax.scatter(poids_centrale[0], poids_centrale[1], marker='.', color='blue')
         ax.scatter(centroids[:, 0], centroids[:, 1], marker='x', color='red')
-        ax.scatter(robot_positions[:, 0], robot_positions[:, 1], marker='o', color='black')
+        ax.scatter(robot_positions[:, 0], robot_positions[:, 1], marker='.', color='black')
+        ax.contour(x, y, z)
         plt.pause(0.01)
 
         create_frame(i)
@@ -85,6 +93,7 @@ x = np.linspace(-2,2,n)
 y = np.linspace(-2,2,n)
 xx, yy = np.meshgrid(x, y)
 data = np.c_[xx.ravel(), yy.ravel()]
+pos = np.dstack((xx, yy))
 
 # Run the Lloyd algorithm with k=10 clusters
 centroids, labels = lloyd_algorithm(data, k=10)
